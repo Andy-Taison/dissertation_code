Added gradient clipping due to nan values observed, and reduced learning rate, quick fix, but suspect issue goes deeper.
Added penalty for difference in padded coordinate voxel count.

src/main.py
@@ -16,7 +16,7 @@ from torch.utils.data import DataLoader, Subset
 def run():
     print("Starting VAE pipeline...\n")
 
-    grid_search_model_name = "full_hi_desc_low_coor"
+    grid_search_model_name = "full_clipped"
     combine_and_save = False  # When false, will load processed files
     use_toy_set = False  # Use 20% of full dataset or full dataset, does not use test set
     testing = False  # 128 samples for train and val sets for quick run testing